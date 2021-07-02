0b8ab7a58a2103cf40e30ff3d87310e2a9de495a
Matias <mrmatiasgomez@gmail.com>
2021-07-02 10:29:51 +0200

    ajout des games

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,45 @@
-const ratioForTabLeft = 0.6;
-const optionForTabLeft = {
-    threshold: ratioForTabLeft,
+// For text Gif 
+
+const ratioForGif = 0.6;
+const optionForGif = {
+    threshold: ratioForGif,
 }
 
-const handleIntersectForTabLeft = function (entriesForTabLeft) {
-    entriesForTabLeft.forEach(function (entryForTabLeft) {
-        if (entryForTabLeft.intersectionRatio > ratioForTabLeft) {
-            entryForTabLeft.target.classList.add('revealGifLeftVisible')
+const handleIntersectForGif = function (entriesForGif) {
+    entriesForGif.forEach(function (entryForGif) {
+        if (entryForGif.intersectionRatio > ratioForGif) {
+            entryForGif.target.classList.add('revealGifRightVisible')
         }
         else {
-            entryForTabLeft.target.classList.remove('revealGifLeftVisible')
+            entryForGif.target.classList.remove('revealGifRightVisible')
         }
     });
 }
 
-const observerForTabLeft = new IntersectionObserver(handleIntersectForTabLeft, optionForTabLeft)
-document.querySelectorAll('.revealGifLeft').forEach(function (m) {
-    observerForTabLeft.observe(m)
+const observerForGif = new IntersectionObserver(handleIntersectForGif, optionForGif)
+document.querySelectorAll('.revealGifRight').forEach(function (m) {
+    observerForGif.observe(m)
+});
+
+// For logo Gif 
+
+const ratio = 0.6;
+const option = {
+    threshold: ratio,
+}
+
+const handleIntersect = function (entries) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+        }
+        else {
+            entry.target.classList.remove('reveal-visible')
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, option)
+document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r)
 });
